7306d27df7b8936deb66b5a73a127f3d85cd45b6
LeeYY26088 <26088@naver.com>
2018-11-09 21:08:11 +0900

    프레임에 이미지넣기와 투명 버튼 설정

--- src/javaProject/BMFrame.java
+++ src/javaProject/BMFrame.java
@@ -7,27 +7,39 @@ import javax.swing.*;
 import java.awt.*;
 
 public class BMFrame extends JFrame{
+
+	ImageIcon i=new ImageIcon("images/FrameTitle.jpg");
+	Image bg=i.getImage();
+
 	public BMFrame() {
 		//현재 화면의 크기 얻기
 		Toolkit kit=Toolkit.getDefaultToolkit();
 		Dimension screen=kit.getScreenSize();
-		
+
 		setTitle("Bomber Man: 봄버 맨");
-		setSize(800,500);//임시 초기화
-		setLocation(screen.width/2, screen.height/2);
+		setSize(1013,637);//게임판 크기
+		setLocation(500, 150);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JPanel panel=new JPanel();//버튼과 이미지 등이 들어갈 패널
-		panel.setLayout(new FlowLayout());//배치관리자 생성: FlowLayout
-		this.add(panel);//프레임에 패널 추가
-		Image img=kit.getImage("BIcon.png");//임시 아이콘
+
+		JPanel background=new JPanel() {
+			public void paintComponent(Graphics g) {
+				g.drawImage(bg, 0, 0, null);
+			}
+		};//버튼과 이미지 등이 들어갈 패널
+		background.setLayout(null);//배치관리자 생성: 절대위치로 배치위해 null
+		this.add(background);//프레임에 패널 추가
+		Image img=kit.getImage("images/Icon.png");//임시 아이콘
 		setIconImage(img);
 		
-		JButton gameStart=new JButton("게임 시작");
-		JButton option=new JButton("옵션");
-		panel.add(gameStart);
-		panel.add(option);
-		
+		ImageIcon gs=new ImageIcon("images/gamestart.png");
+		JButton gameStart=new JButton(gs);
+		gameStart.setBorderPainted(false);
+		gameStart.setContentAreaFilled(false);
+		gameStart.setFocusPainted(false);
+
+		gameStart.setBounds(590, 430, gs.getIconWidth(), gs.getIconHeight());
+		background.add(gameStart);
+
 		setVisible(true);
 	}
 
