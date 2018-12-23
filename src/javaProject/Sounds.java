5566ac879f263e10877f3d7f4ba0844e6db88b59
LeeYY26088 <26088@naver.com>
2018-12-23 22:00:43 +0900

    배경음악 변경

--- src/javaProject/Sounds.java
+++ src/javaProject/Sounds.java
@@ -13,7 +13,7 @@ public class Sounds {
 	
 	public void startBgm() {
 		try{
-			AudioInputStream ais = AudioSystem.getAudioInputStream(new File("sounds/ruins.wav"));
+			AudioInputStream ais = AudioSystem.getAudioInputStream(new File("sounds/sleepywood.wav"));
 			clip=AudioSystem.getClip();
 			clip.open(ais);
 			clip.start();
